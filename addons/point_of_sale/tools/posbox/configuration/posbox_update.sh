3576f44a857a80f3ef6f706322e08a2f1cda9504
Administrator <Administrator@20nuo003-PC>
2017-11-25 15:56:33 +0800

    git更新优化

--- addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
+++ addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo mount -o remount,rw /
+sudo git --work-tree=/home/pi/dotop/ --git-dir=/home/pi/dotop/.git fetch --all 
+sudo git --work-tree=/home/pi/dotop/ --git-dir=/home/pi/dotop/.git reset --hard
 sudo git --work-tree=/home/pi/dotop/ --git-dir=/home/pi/dotop/.git pull
 sudo mount -o remount,ro /
 (sleep 5 && sudo reboot) &
