5b84b0b26ad9772caa7722d3a7aab69a4de726e1
Administrator <Administrator@20nuo003-PC>
2017-11-25 15:32:18 +0800

    取ip优化

--- addons/hw_escpos/controllers/main.py
+++ addons/hw_escpos/controllers/main.py
@@ -195,8 +195,9 @@ class EscposDriver(Thread):
         hosting_ap = os.system('pgrep hostapd') == 0
         ssid = subprocess.check_output('iwconfig 2>&1 | grep \'ESSID:"\' | sed \'s/.*"\\(.*\\)"/\\1/\'', shell=True).rstrip()
         mac = subprocess.check_output('ifconfig | grep -B 1 \'inet addr\' | grep -o \'HWaddr .*\' | sed \'s/HWaddr //\'', shell=True).rstrip()
-        ips =  [ c.split(':')[1].split(' ')[0] for c in commands.getoutput("/sbin/ifconfig").split('\n') if 'inet addr' in c ]
-        #ips =  [ ip for ip in ips if ip not in localips ] 
+        #ips =  [ c.split(':')[1].split(' ')[0] for c in commands.getoutput("/sbin/ifconfig").split('\n') if 'inet addr' in c ]
+        ips = [ c.strip().split(' ')[1] for c in commands.getoutput("/sbin/ifconfig").split('\n') if 'inet ' in c ]
+        ips =  [ ip for ip in ips if ip not in localips ] 
         eprint.text('\n\n')
         eprint.set(align='center',type='b',height=2,width=2)
         eprint.text('PosBox Status\n')
