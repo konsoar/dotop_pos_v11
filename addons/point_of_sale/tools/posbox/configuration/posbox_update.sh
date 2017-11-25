49fafb99ff668f6ef7af8dd60307cc91c9384c23
Administrator <Administrator@20nuo003-PC>
2017-11-25 18:07:13 +0800

    磁盘挂载忙的问题解决

--- addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
+++ addons/point_of_sale/tools/posbox/configuration/posbox_update.sh
@@ -4,5 +4,7 @@ sudo mount -o remount,rw /
 sudo git --work-tree=/home/pi/dotop/ --git-dir=/home/pi/dotop/.git fetch --all 
 sudo git --work-tree=/home/pi/dotop/ --git-dir=/home/pi/dotop/.git reset --hard
 sudo git --work-tree=/home/pi/dotop/ --git-dir=/home/pi/dotop/.git pull
+
+sudo telinit 1
 sudo mount -o remount,ro /
 (sleep 5 && sudo reboot) &
