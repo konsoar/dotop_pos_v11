8c297eb4659eb19c7d184c20ceeb0c42e38b50fa
Administrator <Administrator@20nuo003-PC>
2017-12-14 16:22:40 +0800

    加入网络打印机

---
+++ addons/hw_printer_network/__dotop__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """硬件网络打印机""",
+    "summary": """硬件驱动网络打印机""",
+    "category": "Point of Sale",
+    "version": "10.0",
+    "author": "ERP Team",
+    "website": "http://www.dotopcloud.com",
+    "depends": [
+        "hw_escpos",
+    ],
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "demo": [],
+    "post_load": "post_load",
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
