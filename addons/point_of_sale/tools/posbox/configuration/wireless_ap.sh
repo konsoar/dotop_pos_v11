24d06f7f7f4517d224988633eca4357966c64cc6
konsoar <konsoar@126.com>
2018-01-30 18:27:21 +0800

    强行使用ap模式-02

--- addons/point_of_sale/tools/posbox/configuration/wireless_ap.sh
+++ addons/point_of_sale/tools/posbox/configuration/wireless_ap.sh
@@ -5,7 +5,7 @@ WIRED_IP=$(ifconfig eth0 | grep "inet " | awk -F: '{print $1}' | awk '{print $2}
 WIFI_NETWORK_FILE="/home/pi/wifi_network.txt"
 
 # if there is no wired ip, attempt to start an AP through wireless interface
-if [ -z "${WIRED_IP}" ] ; then
+if [ -z "${WIRED_IP}" || [ -z "${FORCE_HOST_AP}" ]] ; then
 	logger -t posbox_wireless_ap "No wired IP"
 
 	ifconfig wlan0 down
@@ -17,18 +17,23 @@ if [ -z "${WIRED_IP}" ] ; then
 	# we cannot scan for networks while in Master mode
 	# so first scan and save the networks to a list
 	iwlist wlan0 scan | grep 'ESSID:' | sed 's/.*ESSID:"\(.*\)"/\1/' | sort -u > /tmp/scanned_networks.txt
-	
+
 	# only do it when there is a wireless interface
 	if [ -n "$(iw list)" ] ; then
-		logger -t posbox_wireless_ap "Starting AP"
+		if [ -f "${WIFI_NETWORK_FILE}" ] && [ -z "${FORCE_HOST_AP}" ] ; then
+			logger -t posbox_wireless_ap "Loading persistently saved setting"
+			/home/pi/dotop/addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh &
+		else
+			logger -t posbox_wireless_ap "Starting AP"
 
-		service hostapd restart
+			service hostapd restart
 
-		ip addr add 10.11.12.1/24 dev wlan0
+			ip addr add 10.11.12.1/24 dev wlan0
 
-		service isc-dhcp-server restart
+			service isc-dhcp-server restart
 
-		service dotop restart
+			service dotop restart
+		fi
 	# no wired, no wireless
 	else
 		service dotop restart
