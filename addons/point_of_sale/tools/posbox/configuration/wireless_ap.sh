17d600b1d1d47814a3f133bb18df977da02bb11b
konsoar <konsoar@126.com>
2018-01-30 18:18:24 +0800

    强行使用ap模式-01

--- addons/point_of_sale/tools/posbox/configuration/wireless_ap.sh
+++ addons/point_of_sale/tools/posbox/configuration/wireless_ap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FORCE_HOST_AP="FORCE_HOST_AP"
+FORCE_HOST_AP="${1}"
 WIRED_IP=$(ifconfig eth0 | grep "inet " | awk -F: '{print $1}' | awk '{print $2}';)
 WIFI_NETWORK_FILE="/home/pi/wifi_network.txt"
 
@@ -20,20 +20,15 @@ if [ -z "${WIRED_IP}" ] ; then
 	
 	# only do it when there is a wireless interface
 	if [ -n "$(iw list)" ] ; then
-		if [ -f "${WIFI_NETWORK_FILE}" ] && [ -z "${FORCE_HOST_AP}" ] ; then
-			logger -t posbox_wireless_ap "Loading persistently saved setting"
-			/home/pi/dotop/addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh &
-		else
-			logger -t posbox_wireless_ap "Starting AP"
+		logger -t posbox_wireless_ap "Starting AP"
 
-			service hostapd restart
+		service hostapd restart
 
-			ip addr add 10.11.12.1/24 dev wlan0
+		ip addr add 10.11.12.1/24 dev wlan0
 
-			service isc-dhcp-server restart
+		service isc-dhcp-server restart
 
-			service dotop restart
-		fi
+		service dotop restart
 	# no wired, no wireless
 	else
 		service dotop restart
